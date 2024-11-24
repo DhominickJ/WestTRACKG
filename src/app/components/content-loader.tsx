4f041bf240edd17bcec98fbbd383241419cd15eb
DhominickJ <dhominickjohn.billena@wvsu.edu.ph>
2024-11-24 16:13:12 +0800

    Updated for Firebase

---
+++ src/app/components/content-loader.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+
+interface ContentLoaderProps {
+  header: string;
+  content: string;
+  imgSrc: string;
+  imgPos: string;
+}
+
+function ContentLoader({
+  header,
+  content,
+  imgSrc,
+  imgPos,
+}: ContentLoaderProps) {
+  return (
+    <div className="size-full w-full h-500 p-32 bg-white flex items-center">
+      {imgPos === "left" && (
+        <div className="size-full flex items-center justify-center px-10">
+          <Image src={imgSrc} alt="paper-fast-icon" width={500} height={100} />
+          <div className="pr-10">
+            <h3 className="font-bold text-3xl pb-5">{header}</h3>
+            <p className="w-1/2">{content}</p>
+          </div>
+        </div>
+      )}
+      {imgPos === "right" && (
+        <div className="size-full flex items-center justify-center px-10">
+          <div className="pl-10">
+            <h3 className="font-bold text-3xl pb-5">{header}</h3>
+            <p className="w-1/2">{content}</p>
+          </div>
+          <Image
+            src={imgSrc}
+            alt="paper-fast-icon"
+            width={500}
+            height={100}
+            className="right"
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ContentLoader;
